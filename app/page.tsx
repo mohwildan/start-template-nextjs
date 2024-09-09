d4c54c7c6e582515b46d157c6bfa8af85ff238eb
mohwildan <mobafreewtf@gmail.com>
2024-09-09 14:07:27 +0700

    init project

---
+++ app/page.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import React from 'react'
+
+import { Button } from '@/components/ui/button'
+import { Progress } from '@/components/ui/progress'
+
+export default function Home() {
+  // count state
+  const [count, setCount] = React.useState(0)
+
+  // increment count
+  const increment = () => setCount((prev) => prev + 10)
+  return (
+    <main>
+      <div className='h-screen flex items-center justify-center'>
+        <div>
+          <h1 className='font-bold text-3xl text-primary'>
+            Welcome to New App
+          </h1>
+          <div className='text-center mt-8'>
+            <Button onClick={increment}>Click me</Button>
+            <Progress value={count} className='bg-red-500 ' />
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
